3e745a826fb0e35eda395e64e29f4d5fce878f0c
steven Reinart <stevenreinart123@gmail.com>
2020-06-28 21:07:17 -0700

    Updates

---
+++ precache-manifest.59bbb8427356a07e4a14cf9e5c72da4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a1a355273130d3e5c86314f55653b7",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "6faacbe02450392a00e5",
+    "url": "/click-game/static/css/main.dc64fcbc.chunk.css"
+  },
+  {
+    "revision": "0c930e78289b01f21a95",
+    "url": "/click-game/static/js/2.0e3fa562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-game/static/js/2.0e3fa562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6faacbe02450392a00e5",
+    "url": "/click-game/static/js/main.03b36dc4.chunk.js"
+  },
+  {
+    "revision": "53021ac0b94d203cb58d",
+    "url": "/click-game/static/js/runtime-main.46fbd14d.js"
+  }
+]);
